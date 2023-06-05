fc991d4f8ac3f3a77988216ceb44297dab4f2772
kyokoi_japan <kazutakayokoils@gmail.com>
2023-06-05 18:36:38 +0900

    add package(get error)

--- app/index.js
+++ app/index.js
@@ -1,9 +1,19 @@
 import { Link } from "expo-router";
 import { Text } from "react-native";
+import {  GLView } from 'expo-gl';
+import { Renderer } from 'expo-three';
 
 export default function Home() {
   return (<>
     <Text>Home page</Text>
     <Link href="/sample">to sample</Link>
+    <GLView
+      style={{ flex: 1 }}
+      onContextCreate={(gl) => {
+        // Create a WebGLRenderer without a DOM element
+        const renderer = new Renderer({ gl });
+        renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+      }}
+    />
   </>);
 }
